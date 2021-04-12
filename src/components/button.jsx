2cb230eb874e49dcdea36f905dfaa83982bc9eae
eerh <eduardorh1312@gmail.com>
2021-04-12 14:26:09 -0600

    buttons working and wrap w break word

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,16 +1,24 @@
-import  React from 'react';
+import React, { Component } from 'react';
 import './button.css';
 
 const isOperator = val =>{
     return !isNaN(val) || val === "." /*|| val === "="*/ ;
 }
 
-const Button = props =>(
-    <div className ={`button-container ${isOperator(this.props.children) ? null : "operatorButt"}`} onClick={() => this.props.handleClick(this.props.children)}
-    onClick={this.props.addVal}
-    >
-    {this.props.children}</div>
-)
+class Button extends Component {
+    render() {
+        return (
+
+            <div className={`button-container ${isOperator(this.props.children) ? null : "operatorButt"}`} onClick={() => this.props.handleClick(this.props.children)}
+                onClick={this.props.addVal}
+            >
+                {this.props.children}
+            </div>
+
+
+        );
+    }
+};
 
 export default Button;
 
